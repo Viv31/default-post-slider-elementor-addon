79f0c740853a4534e42d01b6022c91e034973b08
Vaibhav Gangrade <45598521+Viv31@users.noreply.github.com>
2023-08-13 18:50:49 +0530

    Update addon.php

--- addon.php
+++ addon.php
@@ -11,7 +11,7 @@
 if(!function_exists('dpsa_register_deafult_post_widget_slider')){
 	function dpsa_register_deafult_post_widget_slider($widgets_manager){
 		require_once( __DIR__ . '/default-post-slider-widget-addon.php' );
-		$widgets_manager->register( new \Elementor_post_Slider_addonWidget() );
+		$widgets_manager->register( new \dpsa_Elementor_post_Slider_addonWidget() );
 	}
 }
 add_action( 'elementor/widgets/register', 'dpsa_register_deafult_post_widget_slider' );
