7c239b028681530e42abb2687b2dbb5fcac78285
Vaibhav Gangrade <45598521+Viv31@users.noreply.github.com>
2023-08-13 18:35:45 +0530

    Update addon.php

--- addon.php
+++ addon.php
@@ -8,13 +8,13 @@
 * Author URL:
 */
  if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
-if(!function_exists('register_deafult_post_widget_slider')){
-	function register_deafult_post_widget_slider($widgets_manager){
+if(!function_exists('dpsa_register_deafult_post_widget_slider')){
+	function dpsa_register_deafult_post_widget_slider($widgets_manager){
 		require_once( __DIR__ . '/default-post-slider-widget-addon.php' );
 		$widgets_manager->register( new \Elementor_post_Slider_addonWidget() );
 	}
 }
-add_action( 'elementor/widgets/register', 'register_deafult_post_widget_slider' );
+add_action( 'elementor/widgets/register', 'dpsa_register_deafult_post_widget_slider' );
 
 
 
